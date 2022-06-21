d04cbaf44faec6b1189d8cf0ae16baea28dc4142
rubenionila <ruben.ionila@cycleon.com>
2022-06-21 15:07:34 +0300

    pagina formular

--- src/main/java/com/licenta/alexandraionila/services/impl/QRGeneratorServiceImpl.java
+++ src/main/java/com/licenta/alexandraionila/services/impl/QRGeneratorServiceImpl.java
@@ -11,7 +11,6 @@ import com.licenta.alexandraionila.entities.Rezervare;
 import com.licenta.alexandraionila.services.EmailService;
 import com.licenta.alexandraionila.services.QRGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
